3951000f7f8590efbcad90c94a19eb0e50d19e10
Vaishalikhachane <113883894+Vaishalikhachane@users.noreply.github.com>
2022-10-03 18:38:14 +0530

    Update MiniProject.java

--- MiniProject.java
+++ MiniProject.java
@@ -44,7 +44,7 @@ public class MiniProject {
 			String Mobile=sc.next();
 			
 			
-			st.executeUpdate("insert into Userinfo2 value ('"+Email+"','"+Fname+"','"+Lname+"','"+Mobile+"')" );
+			st.executeUpdate("insert into Userinfo value ('"+Email+"','"+Fname+"','"+Lname+"','"+Mobile+"')" );
 			
 			System.out.println(" "+Fname+" : Thannku for registration ");
 			
